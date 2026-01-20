4119b403668636f6ee2ffc01ba49f81e6de9c031
Sintu Kumar <sintukumarraj3082@gmail.gom>
2026-01-20 16:55:11 +0530

    updated server config

--- api/index.js
+++ api/index.js
@@ -1,12 +1,15 @@
 import express from "express";
 
+
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 
 mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB connected"))
-  .catch(err => console.log(err));
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log("MongoDB Error:", err));
+
+
 
 const app = express();
 
